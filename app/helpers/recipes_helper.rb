a2991b42c76a1ffa01fb851f359524e63d70b3e9
Nathan Goodman <nmg214@gmail.com>
2018-05-13 13:50:48 -0400

    more favorites

--- app/helpers/recipes_helper.rb
+++ app/helpers/recipes_helper.rb
@@ -1,5 +1,11 @@
 module RecipesHelper
-  def favorite_star(recipe)
-    recipe.favorite? ? "<i class='material-icons md-48 fav fav-auto-check'>star</i>".html_safe : "<i class='material-icons md-48 fav-auto-check'>star_border</i>".html_safe
+  def favorite_star(recipe, size)
+    recipe.favorite? ? "<i class='material-icons md-#{size} fav'>star</i>".html_safe : "<i class='material-icons md-#{size}'>star_border</i>".html_safe
+  end
+
+  def favorite_button(recipe, size)
+    button_to recipe, remote: true, method: :put, params: {recipe: {favorite: !recipe.favorite? } }, class: 'btn btn-default' do
+      favorite_star(recipe,size)
+    end
   end
 end
